c3b7d59596715ead3e2767f99dd01860f923023d
jamesujeon <jamesujeon@gmail.com>
2019-11-07 12:31:10 +0900

    Deployed at 2019-11-07 12:31:10 +0900

--- assets/js/scrollToTop.js
+++ assets/js/scrollToTop.js
@@ -2,21 +2,16 @@ function setupScrollToTopButton() {
   var topButton = $('#scrollToTop');
   topButton.hide();
   topButton.click(function() {
-    topButton.fadeOut(200);
     $('html, body').animate({ scrollTop: 0 }, 200);
     return false;
   });
 
   var lastScrollTop = 0;
   $(window).scroll(function() {
-    if (topButton.is(':animated')) {
-      return;
-    }
-
     if ($(this).scrollTop() > 100 && $(this).scrollTop() < lastScrollTop) {
-      topButton.fadeIn(200);
-    } else {
-      topButton.fadeOut(200);
+      topButton.show();
+    } else if (topButton.is(":visible")) {
+      topButton.hide();
     }
 
     lastScrollTop = $(this).scrollTop();
